89e4b35c78d3752efd01fc4e7775d94da08abf40
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2023-01-31 22:35:17 +0000

    - fix bloom for new rtgl

--- ref/gl/gl_opengl.c
+++ ref/gl/gl_opengl.c
@@ -1002,9 +1002,9 @@ void GL_InitCommands( void )
 	CVAR_DEF_T( rt_me_water_g,				"205",	"water color Green; set by rt_me_water" )
 	CVAR_DEF_T( rt_me_water_b,				"210",	"water color Blue; set by rt_me_water" )
 
-	CVAR_DEF_T( rt_bloom_intensity,			"1",	"bloom intensity" )
-	CVAR_DEF_T( rt_bloom_threshold,			"4",	"bloom threshold" )
-	CVAR_DEF_T( rt_bloom_emis_mult,			"32",	"bloom multiplier for emissive" )
+	CVAR_DEF_T( rt_bloom_intensity,			"0.05",	"bloom intensity" )
+	CVAR_DEF_T( rt_bloom_threshold,			"1",	"bloom threshold" )
+	CVAR_DEF_T( rt_bloom_emis_mult,			"9",	"bloom multiplier for emissive" )
 	CVAR_DEF_T( rt_bloom_dirt,				"2",	"lens dirt intensity" )
 	
 	CVAR_DEF_T( rt_ef_crt,					"0",	"CRT-monitor filter" )
