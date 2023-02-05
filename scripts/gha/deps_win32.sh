f25d30da9ddd078b9a4e04962bd6bd0f1d82697d
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2023-02-05 17:31:40 +0000

    - actions: make a zip containing all the binaries to run the game

--- scripts/gha/deps_win32.sh
+++ scripts/gha/deps_win32.sh
@@ -4,9 +4,12 @@ curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
 
-curl -L "https://github.com/sultim-t/RayTracedGL1/releases/download/v3.0.0-dev/RayTracedGL1-Bundle_050223.zip" > RTGL1.zip
+curl -L "https://github.com/sultim-t/RayTracedGL1/releases/download/v3.0.0-dev/RayTracedGL1-Bundle.zip" > RTGL1.zip
 unzip -q RTGL1.zip -d RTGL1
 
 mkdir -p RTGL1/Build/x64-Release
 mv RTGL1/bin/RayTracedGL1.lib RTGL1/Build/x64-Release/RayTracedGL1.lib
 mv RTGL1/bin/RayTracedGL1.dll RTGL1/Build/x64-Release/RayTracedGL1.dll
+
+curl -L "https://github.com/sultim-t/hlsdk-xash3d/releases/latest/download/hlsdk-master-windows.zip" > HLSDK.zip
+unzip -q HLSDK.zip -d HLSDK
