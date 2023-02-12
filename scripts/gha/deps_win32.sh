9bedd273f1de8f56c1575a7388494fe7b6654fc6
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2023-02-12 12:22:54 +0000

    - set special release for xash-rt

--- scripts/gha/deps_win32.sh
+++ scripts/gha/deps_win32.sh
@@ -4,7 +4,7 @@ curl http://libsdl.org/release/SDL2-devel-$SDL_VERSION-VC.zip -o SDL2.zip
 unzip -q SDL2.zip
 mv SDL2-$SDL_VERSION SDL2_VC
 
-curl -L "https://github.com/sultim-t/RayTracedGL1/releases/download/v3.0.0-dev/RayTracedGL1-Bundle.zip" > RTGL1.zip
+curl -L "https://github.com/sultim-t/RayTracedGL1/releases/download/hl1-dev/RayTracedGL1-Bundle.zip" > RTGL1.zip
 unzip -q RTGL1.zip -d RTGL1
 
 mkdir -p RTGL1/Build/x64-Release
